5a8d86d0d8026349f9a7f722bbdc3b4b14a8ab85
crunchy89 <rocker.hunt@gmail.com>
2020-03-22 20:50:43 +0800

    update db

--- e-learning.sql
+++ e-learning.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Mar 22, 2020 at 12:41 PM
+-- Generation Time: Mar 22, 2020 at 08:49 PM
 -- Server version: 5.7.29-0ubuntu0.18.04.1
 -- PHP Version: 7.2.24-0ubuntu0.18.04.3
 
@@ -40,7 +40,7 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id_user`, `username`, `password`, `id_role`, `is_active`) VALUES
 (1, 'Admin', '$2y$10$qzUVAmoXQZcpo6iNjjaKdOWXIFZq7GI/8zoorTahkgaDFqzl5Z76C', 1, 1),
-(2, 'Ferdy', '$2y$10$pcO3OiCmSrppHTVEQxExQugFF/adnL65Wxfeit4aVRiKR4pInDILS', 1, 1);
+(2, 'Ferdy', '$2y$10$pcO3OiCmSrppHTVEQxExQugFF/adnL65Wxfeit4aVRiKR4pInDILS', 3, 1);
 
 -- --------------------------------------------------------
 
@@ -59,7 +59,15 @@ CREATE TABLE `user_access` (
 --
 
 INSERT INTO `user_access` (`id_access`, `id_menu`, `id_role`) VALUES
-(1, 1, 1);
+(1, 1, 1),
+(19, 2, 2),
+(21, 2, 3),
+(30, 3, 3),
+(35, 2, 1),
+(40, 6, 1),
+(42, 3, 1),
+(43, 7, 1),
+(44, 7, 4);
 
 -- --------------------------------------------------------
 
@@ -71,16 +79,20 @@ CREATE TABLE `user_menu` (
   `id_menu` int(11) NOT NULL,
   `title` varchar(25) NOT NULL,
   `icon` varchar(30) NOT NULL,
-  `is_active` int(11) NOT NULL
+  `is_active` int(11) NOT NULL,
+  `no_order` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user_menu`
 --
 
-INSERT INTO `user_menu` (`id_menu`, `title`, `icon`, `is_active`) VALUES
-(1, 'Admin Menu', 'fas fa-fw fa-laptop', 1),
-(2, 'Menu Guru', 'fas fa-fw fa-user-graduate', 1);
+INSERT INTO `user_menu` (`id_menu`, `title`, `icon`, `is_active`, `no_order`) VALUES
+(1, 'Admin Menu', 'fas fa-fw fa-laptop', 1, 2),
+(2, 'Menu Guru', 'fas fa-fw fa-user-graduate', 1, 3),
+(3, 'Menu TU', 'fas fa-fw fa-file', 1, 4),
+(6, 'Dashboard', 'fas fa-fw fa-tachometer-alt', 1, 1),
+(7, 'Menu Siswa', 'fas fa-fw fa-users', 1, 5);
 
 -- --------------------------------------------------------
 
@@ -99,7 +111,9 @@ CREATE TABLE `user_role` (
 
 INSERT INTO `user_role` (`id_role`, `role`) VALUES
 (1, 'Admin'),
-(2, 'Guru');
+(2, 'Guru'),
+(3, 'TU'),
+(4, 'Siswa');
 
 -- --------------------------------------------------------
 
@@ -123,7 +137,14 @@ CREATE TABLE `user_submenu` (
 INSERT INTO `user_submenu` (`id_submenu`, `id_menu`, `title`, `icon`, `url`, `is_active`) VALUES
 (1, 1, 'User Management', 'fas fa-fw fa-users', 'user', 1),
 (2, 1, 'Role management', 'fas fa-fw fa-cogs', 'role', 1),
-(3, 1, 'Menu Management', 'fas fa-fw fa-code', 'menu', 1);
+(3, 1, 'Menu Management', 'fas fa-fw fa-code', 'menu', 1),
+(6, 1, 'Access Management', 'fas fa-fw fa-lock-open', 'access', 1),
+(7, 2, 'Materi', 'fas fa-fw fa-book', 'materi', 1),
+(12, 6, 'Dashboard', 'fas fa-fw fa-tachometer-alt', 'admin/dashboard', 1),
+(13, 3, 'Data Guru', 'fas fa-fw fa-user', 'guru', 1),
+(14, 3, 'Data Siswa', 'fas fa-fw fa-user', 'siswa', 1),
+(15, 3, 'Data Jurusan', 'fas fa-fw fa-user', 'jurusan', 1),
+(16, 3, 'Data Kelas', 'fas fa-fw fa-user', 'kelas', 1);
 
 --
 -- Indexes for dumped tables
@@ -171,27 +192,27 @@ ALTER TABLE `user_submenu`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `user_access`
 --
 ALTER TABLE `user_access`
-  MODIFY `id_access` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_access` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 --
 -- AUTO_INCREMENT for table `user_menu`
 --
 ALTER TABLE `user_menu`
-  MODIFY `id_menu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_menu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `user_role`
 --
 ALTER TABLE `user_role`
-  MODIFY `id_role` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_role` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `user_submenu`
 --
 ALTER TABLE `user_submenu`
-  MODIFY `id_submenu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_submenu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 --
 -- Constraints for dumped tables
 --
